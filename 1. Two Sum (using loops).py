b4e1f6e9cde350f949a15d30fe569a955b5e4ea9
Mo_Salman <mohamedsalman2006s@gmail.com>
2025-05-04 18:13:38 +0530

    better logic for Two Sum (using loops).py

--- 1. Two Sum (using loops).py
+++ 1. Two Sum (using loops).py
@@ -8,8 +8,8 @@ class Solution(object):
 
 # answer starts from here:
       
-        for i in nums:                           #initialising looping through given list
-            for j in range(1,len(nums)):         #initialising loop 2 to get subsequent elements
-                if i+nums[j]==target:            #checking the two sum condition
-                    list=[nums.index(i), j]      #getting the element indices and storing them in list
-                    return list                  #returning output in specified format
+        for i in range(0, len(num)):                           #initialising looping through given list
+            for j in range(i+1,len(nums)):                     #initialising loop 2 to get subsequent elements
+                if nums[i]+nums[j]==target:                    #checking the two sum condition
+                    list=[i, j]                                #getting the element indices and storing them in list
+                    return list                                #returning output in specified format
